697624d38aa72f7044e867fbb51119a6377e1bab
Kevin Lind <lind@adobe.com>
2023-12-08 20:18:48 -0800

    Git Whisperer commit

--- app/src/main/java/com/example/myapplication/MyFirstApplication.java
+++ app/src/main/java/com/example/myapplication/MyFirstApplication.java
@@ -1,6 +1,19 @@
 package com.example.myapplication;
 
 import android.app.Application;
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Lifecycle;
+import com.adobe.marketing.mobile.Identity;
+import java.utils.Arrays;
 
 public class MyFirstApplication extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+    MobileCore.setApplication(this);
+    MobileCore.configureWithAppID('yourAppId');
+    MobileCore.registerExtension(
+      Arrays.asList(Identity.EXTENSION, Lifecycle.EXTENSION),
+      o -> Log.debug('MyFirstApplication', 'AEP Mobile SDK initialized')
+    );
+  }
 }

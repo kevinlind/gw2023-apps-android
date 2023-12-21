2042e2730f23928eb866ce1bfcd29a19485bb824
casmj <casjohns@adobe.com>
2023-12-21 16:48:07 -0700

    Git Whisperer commit

--- app/src/main/java/com/example/myapplication/MyFirstApplication.java
+++ app/src/main/java/com/example/myapplication/MyFirstApplication.java
@@ -1,6 +1,20 @@
 package com.example.myapplication;
 
 import android.app.Application;
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Identity;
+import com.adobe.marketing.mobile.Lifecycle;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyFirstApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileCore.setApplication(this);
+        List<Class<? extends Extension>> extensions = Arrays.asList(Lifecycle.EXTENSION, Identity.EXTENSION);
+        MobileCore.registerExtensions(extensions, o -> {
+            // Any other post registration processing
+        });
+    }
 }

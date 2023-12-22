25de69d9a9be8c298b78a9fd1ed3e5750dc3f11e
casmj <casjohns@adobe.com>
2023-12-21 17:09:32 -0700

    Git Whisperer commit

--- app/src/main/java/com/example/myapplication/MyFirstApplication.java
+++ app/src/main/java/com/example/myapplication/MyFirstApplication.java
@@ -1,6 +1,29 @@
 package com.example.myapplication;
 
 import android.app.Application;
+import com.adobe.marketing.mobile.*;
+import java.util.HashMap;
 
 public class MyFirstApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileCore.setApplication(this);
+        MobileCore.configureWithAppId("SAMPLE_APP_ID");
+
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("global.privacy", "optedout");
+        MobileCore.updateConfiguration(data);
+
+        try {
+            MobileCore.start(new AdobeCallback() {
+                @Override
+                public void call(Object o) {
+                    MobileCore.configureWithAppId("SAMPLE_APP_ID");
+                }
+            });
+        } catch (InvalidInitException e) {
+            //Log the exception
+        }
+    }
 }
